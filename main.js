0ecabed1b33d2b22a8b6770d740102a612e9cc2f
andrewangelle <andrewangelle@gmail.com>
2017-01-29 22:22:31 -0600

    undid changes to index.html and main.js to debug

--- main.js
+++ main.js
@@ -1,78 +1,27 @@
-function getNameResults(){
-  document.getElementById('instrument-name')
-    .addEventListener('submit', function(event) {
-      event.preventDefault();
+document.getElementById('instrument-form')
+  .addEventListener('submit', function(event) {
+    event.preventDefault();
 
-      var name = document.getElementById('instrument-name').value;
-  
+    var family = document.getElementById('instrument-family').value;
 
-      /**
-      * Fetch JSON data from family API
-      * Convert JSON to JS object
-      * Update results list
-      */
-      fetch('/api/instruments/' + name)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(results) {
-          updateResults(results);
-        });
-    });
-}
+    /**
+     * Fetch JSON data from family API
+     * Convert JSON to JS object
+     * Update results list
+     */
+    fetch('http:localhost:8080/api/family/' + family, { mode : 'no-cors'})
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(results) {
+        updateResults(results);
+      });
+});
 
-
-function getFamilyResults(){
-  document.getElementById('instrument-family')
-    .addEventListener('submit', function(event) {
-      event.preventDefault();
-
-      var family = document.getElementById('instrument-family').value;
-
-      /**
-      * Fetch JSON data from family API
-      * Convert JSON to JS object
-      * Update results list
-      */
-      fetch('/api/family/' + family)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(results) {
-          updateResults(results);
-        });
-    });
-}
-
-
-function getClefResults(){
-  document.getElementById('instrument-clef')
-    .addEventListener('submit', function(event) {
-      event.preventDefault();
-
-      var clef = document.getElementById('instrument-clef').value;
-
-      /**
-      * Fetch JSON data from family API
-      * Convert JSON to JS object
-      * Update results list
-      */
-      fetch('/api/clef/' + clef)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(results) {
-          updateResults(results);
-          console.log(results);
-        });
-    });
-}
-
-
-/**
- * For each result, create a new list item and populate the list item
- * with the name of the instrument, then inject it into the results ul
- */
+  /**
+  * For each result, create a new list item and populate the list item
+  * with the name of the instrument, then inject it into the results ul
+  */
 function updateResults(results) {
   var resultsList = document.getElementById('results');
   resultsList.innerHTML = '';
@@ -87,6 +36,54 @@ function updateResults(results) {
 
 
 
-getNameResults();
+
+
+/*
 getFamilyResults();
 getClefResults();
+
+function getFamilyResults(){
+  document.getElementById('instrument-family').addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      var family = document.getElementById('instrument-family').value;
+
+      /**
+      * Fetch JSON data from family API
+      * Convert JSON to JS object
+      * Update results list
+      
+      fetch('/api/family/' + family)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(results) {
+          updateResults();
+        });
+    });
+}
+
+
+function getClefResults(){
+  document.getElementById('instrument-clef')
+    .addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      var clef = document.getElementById('instrument-clef').value;
+      console.log(clef);
+
+      /**
+      * Fetch JSON data from family API
+      * Convert JSON to JS object
+      * Update results list
+      
+      fetch('/api/clef/' + clef)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(results) {
+          updateResults();
+        });
+
+    });
+}*/
